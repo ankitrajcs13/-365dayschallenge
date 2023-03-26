e3700efdecdd67723f5f24338de14283b723cda4
Ankit Raj <109663828+ankitrajcs13@users.noreply.github.com>
2023-03-26 22:17:23 +0530

    Day 85

---
+++ Day 85/2360_Longest_Cycle_Graph.cpp
@@ -0,0 +1,26 @@
+class Solution {
+ public:
+  int longestCycle(vector<int>& edges) {
+    int ans = -1; // Initialize the answer to -1
+    int time = 1; // Initialize the current time step to 1
+    vector<int> timeVisited(edges.size()); // Initialize a vector to store the time at which each node was first visited
+
+    // Iterate through each node in the graph
+    for (int i = 0; i < edges.size(); ++i) {
+      if (timeVisited[i]) // If the node has already been visited, skip it
+        continue;
+      const int startTime = time; // Record the start time of the current traversal
+      int u = i; // Initialize the current node to the ith node
+      // Traverse the graph until the end of the path is reached or a visited node is encountered
+      while (u != -1 && !timeVisited[u]) {
+        timeVisited[u] = time++; // Record the current time step and increment time
+        u = edges[u]; // Move to the next node in the path
+      }
+      // If a cycle is found that includes the current node, update the answer
+      if (u != -1 && timeVisited[u] >= startTime)
+        ans = max(ans, time - timeVisited[u]);
+    }
+
+    return ans; 
+  }
+};
