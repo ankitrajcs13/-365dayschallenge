3c031dd97a19e1c70c7aef52653d8995c0061e17
Ankit Raj <109663828+ankitrajcs13@users.noreply.github.com>
2023-06-07 22:13:43 +0530

    Day 158

---
+++ Day 158/1318_Min_Flips_To_Make_A_OR_B_Equal.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        
+        int res = 0;
+        while(a | b | c){
+            int fst = a & 1;
+            int scn = b & 1;
+            int thi = c & 1;
+            if ( (fst|scn) != thi){ // it means we have to flip the bits
+                if( fst & scn){ // it means that we have to flip both the bits 
+                    res += 2;
+                }
+                else res += 1;
+            }
+            a >>=1;
+            b >>=1;
+            c >>=1;
+        }
+        return res;
+
+    }
+};
