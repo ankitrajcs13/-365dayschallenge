b6e68d575684c4d110c6b0680dd1a139440c8c8a
Ankit Raj <109663828+ankitrajcs13@users.noreply.github.com>
2023-07-10 22:52:09 +0530

    Day 191

---
+++ Day 191/111_Min_Depth_Binary_Tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int solve(TreeNode* root){
+        if(root == NULL) return 0;
+
+        if(!root->left){
+            return 1 + solve(root->right);
+        }
+        else if(!root->right){
+            return 1 + solve(root->left);
+        }
+        else
+            return 1 + min(solve(root->left), solve(root->right));
+
+    }
+
+    int minDepth(TreeNode* root) {
+        int res = solve(root);
+        return res;
+
+        
+
+    }
+};
