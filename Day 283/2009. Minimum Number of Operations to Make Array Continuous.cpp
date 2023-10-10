4b7508468b8e439436672aa49cfec2bde2d2fd4f
Ankit Raj <ankitrajcs13@gmail.com>
2023-10-10 23:24:59 +0530

    Day 283

---
+++ Day 283/2009. Minimum Number of Operations to Make Array Continuous.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        
+        set<int> st(begin(nums), end(nums));
+        
+        vector<int> temp(begin(st), end(st));
+        
+        int result = INT_MAX;
+        
+        for(int i = 0; i<temp.size(); i++) {
+            
+            int left_range  = temp[i];
+            int right_range = left_range + n - 1;
+            
+            int j = upper_bound(begin(temp), end(temp), right_range) - begin(temp);
+            
+            int in_range  = j - i;
+            int out_range = n - in_range;
+            
+            result = min(result, out_range);
+            
+        }
+        
+        return result;
+    }
+};
